251bae89138f56a5833a6c000cf12629a9bf3810
Jato wangham <118051003+jatowangham@users.noreply.github.com>
2025-05-11 13:18:16 +0000

    changed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let accountEmail = "jato@gmail.com";
+var accountName = "lawrence";
+accountCity = "longding";
+
+// accountId = 123;
+accountEmail = "wang@mail.com";
+accountName = "jack";
+accountCity = "mumbai"
+
+// console.log(accountId);
+console.table([accountEmail, accountName, accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as newer js version
+
+// alert(2 + 2); // cannot run because we're using nodejs not browser
+
+console.log("hello");
+
+// number 
+// bigInt
+// string = "hello"
+// bolean = true/false
+// null = typeof Object
+// undefined = typeof undefined
+// symbol = unique
+
+//object
+
+const ans = null;
+console.log(typeof ans);
