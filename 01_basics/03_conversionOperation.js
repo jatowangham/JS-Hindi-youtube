cc824ef8d9a83a5e657f647b3453d863ad7906fb
Jato wangham <118051003+jatowangham@users.noreply.github.com>
2025-05-11 13:50:37 +0000

    conversion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 55;
+// console.log(typeof score);
+
+// let valueNum = "33a"
+let ansValueNum = Number(score);
+// console.log(typeof ansValueNum, ansValueNum);
+
+// "33" = 33
+// "33sd" = NaN
+// true = 1, false = 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "name" = true
+
+let someNum = 10;
+let stringNumber = String(someNum);
+console.log(typeof stringNumber, stringNumber);
