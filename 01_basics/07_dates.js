0c216817c2c958f36ad00eafef1c6ba201aa3a9f
Jato wangham <118051003+jatowangham@users.noreply.github.com>
2025-05-12 14:07:16 +0000

    updated

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,4 +1,8 @@
 let myDate = new Date();
-console.log(myDate.toString());
-console.log(myDate.t);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
+const myCreatedDate = new Date(2025, 0 ,27);
+console.log(myCreatedDate.toDateString());
